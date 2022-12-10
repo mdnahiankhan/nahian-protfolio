01f206ba281592e8bdf4b8c1b1cc146b80c55bbc
Md. Nahian <102530128+mdnahiankhan@users.noreply.github.com>
2022-12-10 22:52:55 +0600

    added email-js

--- src/Contact/Contact.jsx
+++ src/Contact/Contact.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
-import contact from '../assets/contact me.jpg'
+import contact from '../assets/contact me.jpg';
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+import { toast } from 'react-hot-toast';
 
 const Contact = () => {
-    const HandleSubmit = event => {
-        event.preventDefault();
-        const form = event.target;
-        const name = form.name.value;
-        const email = form.email.value;
-        const message = form.message.value;
-        console.log(name, email, message);
-    }
+
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_xspbpyo', 'template_62p43lu', form.current, 'Vm0eHU7OEx5jAouOF')
+            .then((result) => {
+                console.log(result.text);
+                if (result.text) {
+                    toast.success('Your Email Sent Successfully.')
+                }
+            }, (error) => {
+                console.log(error.text);
+            });
+    };
+
     return (
         <div>
             <div className="grid max-w-screen-xl grid-cols-1 gap-8 px-8 py-16 mx-auto rounded-lg md:grid-cols-2 md:px-12 lg:px-16 xl:px-32  dark:text-gray-900">
@@ -20,18 +32,18 @@ const Contact = () => {
                     </div>
                     <img src={contact} alt="" className="p-6 h-52 md:h-64" />
                 </div>
-                <form onSubmit={HandleSubmit} novalidate="" className="space-y-6 ng-untouched ng-pristine ng-valid">
+                <form ref={form} onSubmit={sendEmail} novalidate="" className="space-y-6 ng-untouched ng-pristine ng-valid">
                     <div>
                         <label for="name" className="text-sm text-white">Full name</label>
-                        <input id="name" name='name' type="text" placeholder="" className="w-full p-3 rounded dark:bg-gray-300" />
+                        <input id="name" name='user_name' type="text" placeholder="" className="w-full p-3 rounded dark:bg-gray-300" required />
                     </div>
                     <div>
                         <label for="email" className="text-sm text-white">Email</label>
-                        <input id="email" name='email' type="email" className="w-full p-3 rounded dark:bg-gray-300" />
+                        <input id="email" name='user_email' type="email" className="w-full p-3 rounded dark:bg-gray-300" required />
                     </div>
                     <div>
                         <label for="message" className="text-sm text-white">Message</label>
-                        <textarea id="message" name='message' rows="3" className="w-full p-3 rounded dark:bg-gray-300"></textarea>
+                        <textarea id="message" required name='message' rows="3" className="w-full p-3 rounded dark:bg-gray-300"></textarea>
                     </div>
                     <button type="submit" className="w-full p-3 text-sm font-bold tracking-wide uppercase rounded dark:bg-violet-400 dark:text-gray-900">Send Message</button>
                 </form>
